7e2af83bbd53d8bcd32464d97142e516a9703de8
Tomo Krajina <tkrajina@gmail.com>
2012-05-15 06:17:05 +0200

    + make.sh

---
+++ bin/make.sh
@@ -0,0 +1,18 @@
+latex analiticka_geometrija.tex
+dvipdf analiticka_geometrija.dvi
+
+latex derivacije_i_integrali.tex
+dvipdf derivacije_i_integrali.dvi
+
+latex geometrijski_likovi.tex
+dvipdf geometrijski_likovi.dvi
+
+latex kompleksni_brojevi.tex
+dvipdf kompleksni_brojevi.dvi
+
+latex salabahter_IV_razred_SS.tex
+dvipdf salabahter_IV_razred_SS.dvi
+
+latex trigonometrija.tex
+dvipdf trigonometrija.dvi
+
